d11441071e4da60a7ee0f95f45527a10cae1fd6e
petrero <petrero@gmail.com>
2024-11-21 11:42:12 +0200

    2.6 Now we can remove this entire switch statement, but we still need to address cases where a button isn’t found. Copy this line here, delete the switch statement entirely, and wrap this press() method in a try-catch block. Move $remote->press($button) into the try, and below, this will catch (NotFoundExceptionInterface). Paste our code inside and... done! So if ContainerInterface::get() doesn’t find a command for the button name, it will throw this exception. Finally, we can add the previous exception to the 404 for better debugging.  Our controller is much smaller now, so let’s test it in our app. If we press the "Power" button... \“Power pressed\” ! If we press the "Channel Up" button... \“Channel up pressed\” ! Everything seems to be working. If we check out the profiler, we can see that the dump() message is still there, and now it's coming from the correct button implementation. Sweet!
     Okay, this looks great, but there’s another improvement we can make. Right now, every time we add a new button, we need to update the AutowireLocator attribute in our ButtonRemote. This is fine, but it's a bit cumbersome.
    
     Next: Let's explore a refactor to remove this requirement.

--- src/Controller/RemoteController.php
+++ src/Controller/RemoteController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Remote\ButtonRemote;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,25 +18,10 @@ final class RemoteController extends AbstractController {
 	): Response {
 		if ('POST' === $request->getMethod()) {
 			$button = $request->request->getString('button');
-			$remote->press($button);
-			switch ($button = $request->request->getString('button')) {
-				case 'power':
-					dump('Power on/off the TV');
-					break;
-				case 'channel-up':
-					dump('Change the channel up');
-					break;
-				case 'channel-down':
-					dump('Change the channel down');
-					break;
-				case 'volume-up':
-					dump('Increase the volume');
-					break;
-				case 'volume-down':
-					dump('Decrease the volume');
-					break;
-				default:
-					throw $this->createNotFoundException(sprintf('Button "%s" not found.', $button));
+			try {
+				$remote->press($button);
+			} catch (NotFoundExceptionInterface $e) {
+				throw $this->createNotFoundException(sprintf('Button "%s" not found.', $button), previous: $e);
 			}
 
 			$this->addFlash('success', sprintf('%s pressed', u($button)->replace('-', ' ')->title(allWords: true)));
