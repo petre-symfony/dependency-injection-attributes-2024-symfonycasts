38f794a2062c66df8a0c01b121765a417a8aab1d
petrero <petrero@gmail.com>
2024-11-22 11:31:28 +0200

    6.2 To actually use this decorator, in RemoteController, instead of injecting ButtonRemote, inject LoggerRemote  Let's try it! Back in our app, press the power button and jump into the profiler for the last request. We can see that the logic from ButtonRemote is still being called. And if we check out the "Logs" panel, we see the messages!

--- src/Controller/RemoteController.php
+++ src/Controller/RemoteController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Remote\ButtonRemote;
+use App\Remote\LoggerRemote;
 use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,7 +14,7 @@ final class RemoteController extends AbstractController {
 	#[Route('/', name: 'home', methods: ['GET', 'POST'])]
 	public function index(
 		Request $request,
-		ButtonRemote $remote
+		LoggerRemote $remote
 	): Response {
 		if ('POST' === $request->getMethod()) {
 			$button = $request->request->getString('button');
