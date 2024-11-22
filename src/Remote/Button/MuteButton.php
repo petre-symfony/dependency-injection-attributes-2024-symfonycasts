b84d0bdac246624099f9402b10eed48f5d8891e9
petrero <petrero@gmail.com>
2024-11-22 11:16:04 +0200

    5.4 To celebrate, let's add a new button to our remote!  Adding a Mute Button  - Create a new PHP class called MuteButton and have it implement ButtonInterface. Press Ctrl+Enter to generate the press() method. Inside, write dump('Pressed mute button'). Now, add #[AsTaggedItem] with an  of mute. Leave the priority as the default, 0. This will slot this button below the others

---
+++ src/Remote/Button/MuteButton.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Remote\Button;
+
+use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+
+#[AsTaggedItem('mute')]
+class MuteButton implements ButtonInterface {
+
+	public function press(): void {
+		dump('Mute button pressed');
+	}
+}
