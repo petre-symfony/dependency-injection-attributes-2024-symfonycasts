62265b6e4d7091db8b01e4ce48dc3892258f0336
petrero <petrero@gmail.com>
2024-11-23 21:56:44 +0200

    9.2 #[When]  - Our new button is automatically registered with the service container, but we want it only in the dev environment. The #[When] attribute is perfect for this. Back in DiagnosticsButton, add #[When] with dev as the argument
     Thanks to this, the class will be completely ignored by the service container unless we're in the dev environment. Head over and refresh. It's still there. That makes sense: we are in the dev environment.

--- src/Remote/Button/DiagnosticsButton.php
+++ src/Remote/Button/DiagnosticsButton.php
@@ -4,8 +4,10 @@ namespace App\Remote\Button;
 
 use App\Remote\RemoteInterface;
 use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
+use Symfony\Component\DependencyInjection\Attribute\When;
 
 #[AsTaggedItem('diagnostics')]
+#[When('dev')]
 class DiagnosticsButton implements ButtonInterface {
 	public function press(): void {
 		dump('Press diagnostics button.');
