b71ab5923c95ec1c9a01ddf071bd99ebca70bae8
petrero <petrero@gmail.com>
2024-11-23 22:22:19 +0200

    10.2 Next, open up VolumeUpButton, add a constructor here and inject private ParentalControls $parentalControls
     In the press() method, pretend we're detecting when the volume is too high. Add an if (true) statement (with a comment to remind us what this represents), then $this->parentalControls->volumeTooHigh()
    
     Spin over to our app, refresh, press the "volume up" button, and check the profiler. We can see our ParentalControls service is being used and working!

--- src/Remote/Button/VolumeUpButton.php
+++ src/Remote/Button/VolumeUpButton.php
@@ -2,11 +2,22 @@
 
 namespace App\Remote\Button;
 
+use App\Remote\ParentalControls;
 use Symfony\Component\DependencyInjection\Attribute\AsTaggedItem;
 
 #[AsTaggedItem('volume-up', priority: 20)]
 final class VolumeUpButton implements ButtonInterface {
+	public function __construct(
+		private ParentalControls $parentalControls
+	) {
+	}
+
 	public function press(): void {
+		if (true) {
+			//determine the volume is too high
+			$this->parentalControls->volumeTooHigh();
+		}
+
 		dump('Change the volume up');
 	}
 }
