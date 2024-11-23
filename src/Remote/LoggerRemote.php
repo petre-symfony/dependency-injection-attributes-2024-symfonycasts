63b1037e05d449e7585eb9a9198ac7a8f100efe8
petrero <petrero@gmail.com>
2024-11-23 21:12:13 +0200

    8.3 Named Autowiring  - In LoggerRemote, rename the argument to $buttonLogger
     Now, head back to our app... refresh the page, and... press the "Channel Up" button. We've watched enough Sponge Bob for one day. Pop into the last request's profile and check out the "Logs" panel. Sweet! We're now logging to the button channel!

--- src/Remote/LoggerRemote.php
+++ src/Remote/LoggerRemote.php
@@ -8,19 +8,19 @@ use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 #[AsDecorator(ButtonRemote::class)]
 class LoggerRemote implements RemoteInterface{
 	public function __construct(
-		private LoggerInterface $logger,
+		private LoggerInterface $buttonLogger,
 		private RemoteInterface $inner
 	) {
 	}
 
 	public function press(string $name): void {
-		$this->logger->info('Pressing button {name}', [
+		$this->buttonLogger->info('Pressing button {name}', [
 			'name' => $name
 		]);
 
 		$this->inner->press($name);
 		
-		$this->logger->info('Pressed button {name}', [
+		$this->buttonLogger->info('Pressed button {name}', [
 			'name' => $name
 		]);
 	}
